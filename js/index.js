f95f84c618295141e7e0d7cc1fe701aa6072970a
jryuan <yaojerry2000@gmail.com>
2022-06-06 16:07:22 +0800

    index-animate-fix

--- js/index.js
+++ js/index.js
@@ -17,8 +17,8 @@ function update() {
     let tiltx = (dy / cy );
     let tilty = - (dx / cx);
 
-TweenMax.to("#container dec", 1, {x:-tilty*20, y:-tiltx*20, rotation:0.01, ease:Power2.easeOut});
-TweenMax.to("#container liz", 1, {x:tilty*20, y:tiltx*20, rotation:0.01, ease:Power2.easeOut});
+TweenMax.to("#container dec", 1, {x:-tilty*15, y:-tiltx*15, rotation:0.01, ease:Power2.easeOut});
+TweenMax.to("#container liz", 1, {x:-tilty*20, y:-tiltx*20, rotation:0.01, ease:Power2.easeOut});
 }
 
 
