ccf75a4a964316757780e7de313a1dcf074f8336
Jryuann <103395895+Jryuann@users.noreply.github.com>
2022-06-09 22:55:07 +0800

    part1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0,0);
+}
+
 let wrap = document.getElementById('part_index');
 let request = null;
 let mouse = { x: 0, y: 0 };
@@ -17,9 +21,9 @@ function update() {
     let tiltx = (dy / cy );
     let tilty = - (dx / cx);
 
-TweenMax.to("#container dec", 1, {x:-tilty*20   , y:-tiltx*20  , rotation:0.01, ease:Power2.easeOut});
-TweenMax.to("#container liz", 1, {x:-tilty*-20, y:-tiltx*-20, rotation:0.01, ease:Power2.easeOut});
-TweenMax.to("#container bg", 1, {x:-tilty*-10, y:-tiltx*-10, rotation:0.01, ease:Power2.easeOut});
+TweenMax.to("#container dec", 0.5, {x:-tilty*-100   , y:-tiltx*-100  , rotation:360, ease:Power0.easeNone});
+TweenMax.to("#container liz", 1, {x:-tilty*-100, y:-tiltx*-100, rotation:360, ease:Power0.easeNone});
+TweenMax.to("#container bg", 1, {x:-tilty*-100, y:-tiltx*-100, rotation:360, ease:Power0.easeNone});
 
 }
 
@@ -28,3 +32,34 @@ window.addEventListener('resize', function(event){
  window.innerWidth / 2;
    window.innerHeight / 2;
 });
+// 下面新作法 
+// function touches(e){
+// 	var x = e.touches ? e.touches[0].clientX : e.clientX, 
+// 			y = e.touches ? e.touches[0].clientY : e.clientY;
+//   var w = window.innerWidth / 2;
+//   var h = window.innerHeight / 2;
+  
+//   var l = -(x - w) / (w / 10) - 10;
+//   var t = -(y - h) / (h / 10) - 10;
+//       //10 / (y - (h / 2)) * 100;             
+//   console.log(y + ' | ' + h + ' | ' + t);
+  
+// 	TweenMax.to($('.decs'),0.1, {
+// 		top: t/2 + "%",
+// 		left: l/2 + "%"
+// 	});
+
+//     TweenMax.to($('.liz_dec'), 3, {
+// 		top: -t + "%",
+// 		left: -l + "%"
+// 	});
+//     TweenMax.to($('.bg'), 3, {
+// 		top: t + "%",
+// 		left: l + "%"
+// 	});
+	
+// }
+
+// window.addEventListener("mousemove", touches);
+// window.addEventListener("touchstart", touches);
+// window.addEventListener("touchmove", touches);
--- js/index_loading_animation.js
+++ js/index_loading_animation.js
@@ -1,4 +1,6 @@
+
 document.addEventListener("DOMContentLoaded", function (event) {
+
 
 
     gsap.to(".index_start", {
@@ -8,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         x: 0,
         y: -1000,
         duration: 3,
+        ease:Power0.easeNone,
         // ease: "back",
         // stagger: {
         //     each: 0.3,
@@ -18,30 +21,74 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     setInterval(function () {
         gsap.to(".index_start", {
-
-            
             style: "display:none;"
-
+        });
+        gsap.to("body", {
+            style: "overflow: scroll; overflow-x: hidden"
         });
     }, 4000);
 
-    gsap.to("#container liz", {
+    gsap.to("#container .liz_dec", {
         rotation: 360,
-        // opacity: 1,
+        opacity: 1,
         scale: 1,
-        x: 0,
-        y: 0,
+        // x: 0,
+        // y: 0,
         duration: 1.5,
-        delay: 1,
+        delay: 1.5,
+        // top: -10,
+        // left: -10   
 
-        // ease: "back",
+        ease:Power0.easeNone,
         // stagger: {
         //     each: 0.3,
         //     from: "end",
         // },
     });
 
+    gsap.to("#container .inner", {
+        rotation: 360,
+        opacity: 1,
+        scale: 1,
+
+        duration: 1.5,
+        delay: 2,
+    });
+    gsap.to("#container .load_animate", {
+        rotation: 360,
+        opacity: 1,
+        scale: 1,
+
+        duration: 1.5,
+        delay: 2,
+
+        stagger: {
+            each: 0.1,
+            from: "end",
+        },
+
+    });
+
+    gsap.to("#container .load_animate_opacity", {
+        opacity: 1,
+        duration: 1,
+        delay: 2.5 ,
+    });
 
 
 
 });
+
+
+// document.querySelectorAll("nav li").forEach((btn, index) => {
+//     btn.addEventListener("click", () => {
+//       gsap.to(window, {duration: 1, scrollTo:{y:"#part_" + (index + 1), offsetY:70}});
+//     });
+//   });
+
+const main_liz = document.querySelector(".main_liz")
+
+main_liz.addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo: "#part_2"})
+})
+  
