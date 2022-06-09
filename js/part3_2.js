c016080d6243f76877a24f47e8eb4db4c067decf
scarltt924 <95957388+scarltt924@users.noreply.github.com>
2022-06-09 10:36:33 +0800

    part3_0609_2

--- js/part3_2.js
+++ js/part3_2.js
@@ -1,30 +1,40 @@
-gsap.timeline({
+        // let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".wrapper",
+//     scrub:0.1,
+//     start: "top center"
+//   }
+// })
 
+let arr = gsap.utils.toArray(".wrapper")
+
+arr.forEach((e) => {
+  console.log(e.children[0])
+  let tl = gsap.timeline({
     scrollTrigger: {
-        trigger: 'section',
-        pin: true,
-        start: 'center bottom',
-        end: '+=3250', // = 'total time'
-        scrub: true,
-        scale: 0,
-        y: "-=2000",
+      trigger: e,
+      scrub:0,
+      end: '+=2200',
+      start: "top bottom"
     }
+  })
+  
+    tl.to(e.children[0], {
+    scale: 12,
+
+    })
+
+    tl.to(e.children[0], {
+      scale: 2,
+
+    })
 })
+// tl.to("#back", {
+//   scale: 10,
 
-    .to('.o', {
-        //scale:100, // to do: calc of value in relation to window.innerWidth / .innerHeigt
-        scale: 100,
-        duration: 1.2, ease: 'none',
-        stagger: 0.1, // 
+// })
 
-    })
-
-
-    .to('.o', {
-        //scale:100, // to do: calc of value in relation to window.innerWidth / .innerHeigt
-        scale: 1,
-        duration: 0.3, ease: 'none',
-        stagger: 0.1, // 
-        y: "-=2000",
-    })
-
+// tl.to("#back", {
+//   scale: 2,
+  
+// })
