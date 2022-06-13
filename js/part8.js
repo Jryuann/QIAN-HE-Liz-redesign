15ce6a53d2bbfdfc52cc899ac53a4829e6cdbadf
Inshock111 <aids527@gmail.com>
2022-06-13 14:40:51 +0800

    I

--- js/part8.js
+++ js/part8.js
@@ -12,10 +12,31 @@ let tl = gsap.timeline({
       scrub: true,
       end : "100%-=1500px bottom" ,
       markers: true,
-      // stagger: 0.5,
   }
 });
 tl.to("#step-1-square", { x: 0, duration: 6, opacity:1, scale:1})
 .to("#step-2-square", { x: 0, duration: 6, opacity:1, scale:1 })
 .to("#step-3-square", { x: 0, duration: 6, opacity:1, scale:1 })
 .to("#step-4-square", { x: 0, duration: 6, opacity:1, scale:1 })
+
+
+// gsap.set("#step-1-square", { x: 200, y: 0 , opacity:0, scale:0.2})
+// gsap.set("#step-2-square", { x: -200, y: 0, opacity:0, scale:0.2 })
+// gsap.set("#step-3-square", { x: 200, y: 0, opacity:0, scale:0.2 })
+// gsap.set("#step-4-square", { x:-200, y: 0, opacity:0, scale:0.2 })
+
+
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//       trigger: ".part_8 " + " .part_9 ",
+//       start: "top top",
+//       pin: true,
+//       scrub: true,
+//       end : "bottom bottom+=1500" ,
+//       markers: true,
+//   }
+// });
+// tl.to("#step-1-square", { x: 0, duration: 6, opacity:1, scale:1})
+// .to("#step-2-square", { x: 0, duration: 6, opacity:1, scale:1 })
+// .to("#step-3-square", { x: 0, duration: 6, opacity:1, scale:1 })
+// .to("#step-4-square", { x: 0, duration: 6, opacity:1, scale:1 })
