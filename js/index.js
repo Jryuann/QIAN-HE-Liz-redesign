f6c5c29d12730983ba1db5dd1d8dab4c7e469d7a
Jryuann <103395895+Jryuann@users.noreply.github.com>
2022-06-14 22:15:56 +0800

    首頁JS

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0,0);
-}
-window.onload = function() {
-  if(document.body.scrollTop > 0) {
-      console.log(1);
-      window.scrollTo(0, -1);
-      document.body.scrollTop = 0;
+window.onload = function () {
+  gsap.to("body", {
+    style: "overflow:hidden;"
+  });
+  if (document.body.scrollTop > 0) {
+    console.log(1);
+    window.scrollTo(0, -1);
+    document.body.scrollTop = 0;
   }
   window.scrollTo(0, -1);
   document.body.scrollTop = 0;
+
 }
-// 上面是重新整理還有仔入會回頂部
+// 上面是重新整理還有載入時會回頂部，然後把滾動條鎖住
 
 let wrap = document.querySelector('.part_index');
 let request = null;
@@ -18,29 +19,29 @@ let mouse = { x: 0, y: 0 };
 let cx = window.innerWidth / 2;
 let cy = window.innerHeight / 2;
 
-wrap.addEventListener('mousemove', function(event) {
-    mouse.x = event.pageX;
-    mouse.y = event.pageY;
-cancelAnimationFrame(request);
-request = requestAnimationFrame(update);
+wrap.addEventListener('mousemove', function (event) {
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+  cancelAnimationFrame(request);
+  request = requestAnimationFrame(update);
 });
 
 function update() {
-    dx = mouse.x - cx;
-    dy = mouse.y - cy;
-    let tiltx = (dy / cy );
-    let tilty = - (dx / cx);
+  dx = mouse.x - cx;
+  dy = mouse.y - cy;
+  let tiltx = (dy / cy);
+  let tilty = - (dx / cx);
 
 
 
-  gsap.to("#container dec", 2, {x:-tilty*20, y:-tiltx*20  , rotation:360});
-  gsap.to("#container liz", 2, {x:tiltx*-25, y:tilty*-15, rotation:360});
-  gsap.to("#container bg", 2, {x:-tilty*20, y:-tiltx*20, rotation:360});
+  gsap.to("#container dec", 2, { x: -tilty * 20, y: -tiltx * 20, rotation: 360 });
+  gsap.to("#container liz", 2, { x: tiltx * -25, y: tilty * -15, rotation: 360 });
+  gsap.to("#container bg", 2, { x: -tilty * 20, y: -tiltx * 20, rotation: 360 });
 
 }
 
 
-window.addEventListener('resize', function(event){
- window.innerWidth / 2;
-   window.innerHeight / 2;
+window.addEventListener('resize', function (event) {
+  window.innerWidth / 2;
+  window.innerHeight / 2;
 });
