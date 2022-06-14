1b6002e4c25bcd3367c21852a6296aad8f3c649f
scarltt924 <95957388+scarltt924@users.noreply.github.com>
2022-06-15 02:29:42 +0800

    TEST

--- js/part4.js
+++ js/part4.js
@@ -1,7 +1,44 @@
 
-gsap.set("#card1", { x: - window.innerWidth * -2, y: 0 })
-gsap.set("#card2", { x: - window.innerWidth * -2, y: 0 })
-gsap.set("#card3", { x: - window.innerWidth * -2, y: 0 })
+// gsap.set("#card1", { x: - window.innerWidth * -2, y: 0 })
+// gsap.set("#card2", { x: - window.innerWidth * -2, y: 0 })
+// gsap.set("#card3", { x: - window.innerWidth * -2, y: 0 })
+
+// ScrollTrigger.matchMedia({
+//   "(min-width: 600px)": () => {
+//     gsap.registerPlugin(ScrollTrigger);
+//     let part3_t1 = gsap.timeline({
+//       scrollTrigger: {
+//         trigger: ".part_4",
+//         start: 2800,
+//         end: "bottom-=100 bottom",
+//         scrub: true,
+//         // markers: true,
+
+//         duration: 5,
+//       },
+//     })
+//     part3_t1.to("#card1", { x: 0 })
+//     part3_t1.to("#card2", { x: 0, delay: 2 })
+//     part3_t1.to("#card3", { x: 0, delay: 2 })
+//   },
+//   "(min-width: 100px)": () => {
+//     gsap.registerPlugin(ScrollTrigger);
+//     let part3_t1 = gsap.timeline({
+//       scrollTrigger: {
+//         trigger: ".part_4",
+//         start: "top-=800 top",
+//         end: "bottom-=100 bottom",
+//         scrub: true,
+//         // markers: true,
+
+//         duration: 5,
+//       },
+//     })
+//     part3_t1.to("#card1", { x: 0 })
+//     part3_t1.to("#card2", { x: 0, delay: 2 })
+//     part3_t1.to("#card3", { x: 0, delay: 2 })
+//   }
+// })
 
 ScrollTrigger.matchMedia({
   "(min-width: 600px)": () => {
@@ -17,27 +54,14 @@ ScrollTrigger.matchMedia({
         duration: 5,
       },
     })
+    gsap.set("#card1", { x: - window.innerWidth * -2, y: 0 })
+    gsap.set("#card2", { x: - window.innerWidth * -2, y: 0 })
+    gsap.set("#card3", { x: - window.innerWidth * -2, y: 0 })
     part3_t1.to("#card1", { x: 0 })
     part3_t1.to("#card2", { x: 0, delay: 2 })
     part3_t1.to("#card3", { x: 0, delay: 2 })
   },
-  "(min-width: 100px)": () => {
-    gsap.registerPlugin(ScrollTrigger);
-    let part3_t1 = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".part_4",
-        start: "top-=800 top",
-        end: "bottom-=100 bottom",
-        scrub: true,
-        // markers: true,
 
-        duration: 5,
-      },
-    })
-    part3_t1.to("#card1", { x: 0 })
-    part3_t1.to("#card2", { x: 0, delay: 2 })
-    part3_t1.to("#card3", { x: 0, delay: 2 })
-  }
 })
 
 
