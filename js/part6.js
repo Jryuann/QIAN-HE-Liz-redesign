d334e7756efba815441875654d58e79fb90f1726
scarltt924 <95957388+scarltt924@users.noreply.github.com>
2022-06-14 22:46:06 +0800

    0614_33

--- js/part6.js
+++ js/part6.js
@@ -7,39 +7,59 @@
 // gsap.set(" .card-area-text ", { x: 0, y: 500, opacity: 0 })
 gsap.registerPlugin(ScrollTrigger);
 
-const blocks = document.querySelectorAll(".card-area");
+// const blocks = document.querySelectorAll(".card-area");
 
-blocks.forEach(block => {
-  const blockTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: block,
-      start: "top-50% top",
-      end: "bottom center",
-      toggleActions: "play reverse play reverse",
-      markers: true
-    }
-  })
+// blocks.forEach(block => {
+//   const blockTimeline = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: block,
+//       start: "top-50% top",
+//       end: "bottom center",
+//       toggleActions: "play reverse play reverse",
+//       markers: true
+//     }
+//   })
 
-  // Animate the content items
-  const items = block.querySelectorAll(".sq");
-  blockTimeline.from(items, {
-    duration: 1,
-    x: 40,
-    opacity: 0,
-    stagger: .25,
-  }, 0)
+//   // Animate the content items
+//   const items = block.querySelectorAll(".sq");
+//   blockTimeline.from(items, {
+//     duration: 1,
+//     x: 40,
+//     opacity: 0,
+//     stagger: .25,
+//   }, 0)
   
-  // Animate the ps in the items
-  items.forEach((item, i) => {
-    blockTimeline.from(item.querySelectorAll(".card-area-text"), {
-      duration: 1,
-      y: 30,
-      opacity: 0,
-      stagger: .25
-    }, i * .25);
-  });
+//   // Animate the ps in the items
+//   items.forEach((item, i) => {
+//     blockTimeline.from(item.querySelectorAll(".card-area-text"), {
+//       duration: 1,
+//       y: 30,
+//       opacity: 0,
+//       stagger: .25
+//     }, i * .25);
+//   });
+// });
+
+
+gsap.set(".sq", { x: 40 , opacity : 0 })
+gsap.set(".card-area-text", {y : 40 , opacity : 0 })
+
+gsap.registerPlugin(ScrollTrigger);
+let part6_t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part_6",
+    start: 6500,
+    end: "bottom-=200 bottom",
+    scrub: true,
+    markers: true,
+
+    duration: 5,
+  },
 });
 
+part6_t1.to(".card-area", {x: 0})
+part6_t1.to(".sq", {x : 0 , y : 0 , opacity : 1 , duration : 1 , delay : 0.1 })
+part6_t1.to(".card-area-text", { y : 0 , opacity: 1 , duration : 1})
 
     // text
     // gsap.set(" #card-area-text1 ", { x: 0, y: 500, opacity: 0 })
