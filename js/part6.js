741e41dc42273fa8fe970c528003162738f00219
Inshock111 <aids527@gmail.com>
2022-06-14 14:00:17 +0800

    I

---
+++ js/part6.js
@@ -0,0 +1,103 @@
+// gsap.set(" #card-area-square1 ", { x: 0, y: 0, opacity: 0, scale: 0.1, })
+// gsap.set(" #card-area-square2 ", { x: 0, y: 0, opacity: 0, scale: 0.1, })
+// gsap.set(" #card-area-square3 ", { x: 0, y: 0, opacity: 0, scale: 0.1, })
+// gsap.set(" #card-area-square4 ", { x: 0, y: 0, opacity: 0, scale: 0.1, })
+
+// gsap.set(" .sq ", { x: 0, y: 0, opacity: 0, scale: 0.1, })
+// gsap.set(" .card-area-text ", { x: 0, y: 500, opacity: 0 })
+gsap.registerPlugin(ScrollTrigger);
+
+const blocks = document.querySelectorAll(".card-area");
+
+blocks.forEach(block => {
+  const blockTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: block,
+      start: "top-5% top",
+      end: "bottom center",
+      toggleActions: "play reverse play reverse",
+      markers: true
+    }
+  })
+
+  // Animate the content items
+  const items = block.querySelectorAll(".sq");
+  blockTimeline.from(items, {
+    duration: 1,
+    x: 40,
+    opacity: 0,
+    stagger: .25,
+  }, 0)
+  
+  // Animate the ps in the items
+  items.forEach((item, i) => {
+    blockTimeline.from(item.querySelectorAll(".card-area-text"), {
+      duration: 1,
+      y: 30,
+      opacity: 0,
+      stagger: .25
+    }, i * .25);
+  });
+});
+
+
+    // text
+    // gsap.set(" #card-area-text1 ", { x: 0, y: 500, opacity: 0 })
+    // gsap.set(" #card-area-text2 ", { x: 0, y: 500, opacity: 0 })
+    // gsap.set(" #card-area-text3 ", { x: 0, y: 500, opacity: 0 })
+    // gsap.set(" #card-area-text4 ", { x: 0, y: 500, opacity: 0 })
+
+    // ScrollTrigger.matchMedia({
+    //     "(max-width: 1920px)": () => {
+    //         let tl2 = gsap.timeline({
+    //             scrollTrigger: {
+    //                 trigger: ".part_6 " + ".part_7",
+    //                 start: "top top",
+    //                 pin: true,
+    //                 scrub: true,
+    //                 end: "100%-=4200px bottom",
+    //                 markers: true,
+    //             }
+    //         });
+
+            // tl2.to(" #card-area-square1", { x: 0, duration: 50, opacity: 1, scale: 1, })
+            //     .to(" #card-area-text1", { y: 0, duration: 50, opacity: 1, })
+
+            //     .to(" #card-area-square2", { x: 0, duration: 50, opacity: 1, scale: 1, })
+            //     .to(" #card-area-text2", { y: 0, duration: 50, opacity: 1, })
+
+            //     .to(" #card-area-square3", { x: 0, duration: 50, opacity: 1, scale: 1, })
+            //     .to(" #card-area-text3", { y: 0, duration: 50, opacity: 1, })
+
+            //     .to(" #card-area-square4", { x: 0, duration: 50, opacity: 1, scale: 1, })
+            //     .to(" #card-area-text4", { y: 0, duration: 50, opacity: 1, })
+
+        //     tl2.to(" .sq ", { x: 0, duration: 50, opacity: 1, scale: 1 })
+        //         .to(" .card-area-text", { y: 0, duration: 50, opacity: 1 })
+
+        // },
+
+    //     "(max-width: 930px)": () => {
+    //         let tl2 = gsap.timeline({
+    //             scrollTrigger: {
+    //                 trigger: ".part_6" + ".part_7",
+    //                 start: "top top",
+    //                 pin: true,
+    //                 scrub: true,
+    //                 end: "100%-=3500px bottom",
+    //                 markers: true,
+    //             }
+    //         });
+    //         tl2.to(" #card-area-square1", { x: 0, duration: 50, opacity: 1, scale: 1, })
+    //             .to(" #card-area-text1", { y: 0, duration: 50, opacity: 1, })
+
+    //             .to(" #card-area-square2", { x: 0, duration: 50, opacity: 1, scale: 1, })
+    //             .to(" #card-area-text2", { y: 0, duration: 50, opacity: 1, })
+
+    //             .to(" #card-area-square3", { x: 0, duration: 50, opacity: 1, scale: 1, })
+    //             .to(" #card-area-text3", { y: 0, duration: 50, opacity: 1, })
+
+    //             .to(" #card-area-square4", { x: 0, duration: 50, opacity: 1, scale: 1, })
+    //             .to(" #card-area-text4", { y: 0, duration: 50, opacity: 1, })
+    //     }
+    // })
--- js/part8.js
+++ js/part8.js
@@ -1,23 +1,70 @@
-gsap.set("#step-1-square", { x: 200, y: 0 , opacity:0, scale:0.2})
-gsap.set("#step-2-square", { x: -200, y: 0, opacity:0, scale:0.2 })
-gsap.set("#step-3-square", { x: 200, y: 0, opacity:0, scale:0.2 })
-gsap.set("#step-4-square", { x:-200, y: 0, opacity:0, scale:0.2 })
+// 文字方塊
+// gsap.set("#step-1-square", { x: 200, y: 0, opacity: 0, scale: 0.2 })
+// gsap.set("#step-2-square", { x: -200, y: 0, opacity: 0, scale: 0.2 })
+// gsap.set("#step-3-square", { x: 200, y: 0, opacity: 0, scale: 0.2 })
+// gsap.set("#step-4-square", { x: -200, y: 0, opacity: 0, scale: 0.2 })
+// // line 
+// gsap.set(".join-step-line", { x: 0, y: -100, opacity: 0, scale: 0.1 })
+// gsap.set(".contract-step-line", { x: 0, y: -100, opacity: 0, scale: 0.1 })
+// gsap.set(".tec-step-line", { x: 0, y: -100, opacity: 0, scale: 0.1 })
+
+// 930px
+// gsap.set("#square-group-1", { x: 0, y: -100, opacity: 0.1, scale: 0.1 })
+// gsap.set("#square-group-2", { x: 0, y: -100, opacity: 0.1, scale: 0.1 })
+// gsap.set("#square-group-3", { x: 0, y: -100, opacity: 0.1, scale: 0.1 })
+// gsap.set("#square-group-4", { x: 0, y: -100, opacity: 0.1, scale: 0.1 })
 
 
-let tl = gsap.timeline({
-  scrollTrigger: {
-      trigger: ".part_8 "+".part_9",
-      start: "+=2000px top",
-      pin: true,
-      scrub: true,
-      end : "100%-=1500px bottom" ,
-      markers: true,
-  }
-});
-tl.to("#step-1-square", { x: 0, duration: 6, opacity:1, scale:1})
-.to("#step-2-square", { x: 0, duration: 6, opacity:1, scale:1 })
-.to("#step-3-square", { x: 0, duration: 6, opacity:1, scale:1 })
-.to("#step-4-square", { x: 0, duration: 6, opacity:1, scale:1 })
+// ScrollTrigger.matchMedia({
+//   "(max-width: 1920px)": () => {
+//     let tl = gsap.timeline({
+//       scrollTrigger: {
+//         trigger: ".part_8 " + ".part_9",
+//         start: "+=2200px top",
+//         pin: true,
+//         scrub: true,
+//         end: "100%-=1500px bottom",
+//         markers: true,
+//       }
+//     });
+//     tl.to("#step-1-square", { x: 0, duration: 50, opacity: 1, scale: 1 })
+//       // line
+//       .to(".join-step-line", { y: 0, duration: 50, opacity: 1, scale: 1 })
+
+//       .to("#step-2-square", { x: 0, duration: 50, opacity: 1, scale: 1 })
+//       // line
+//       .to(".contract-step-line", { y: 0, duration: 50, opacity: 1, scale: 1 })
+
+//       .to("#step-3-square", { x: 0, duration: 50, opacity: 1, scale: 1 })
+//       // line
+//       .to(".tec-step-line", { y: 0, duration: 50, opacity: 1, scale: 1 })
+
+//       .to("#step-4-square", { x: 0, duration: 50, opacity: 1, scale: 1 })
+//   }
+
+// "(max-width: 930px)": () => {
+//   let tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".part_8" + ".part_9" ,
+//         start: "+=2200px top",
+//         pin: true,
+//         scrub: true,
+//         end: "100%-=1500px bottom",
+//         markers: true,
+//     }
+// });
+
+// tl.to("#square-group-1", { y: 0, duration: 100, opacity: 1, scale: 1 })
+//     // line
+//     .to("#square-group-2", { y: 0, duration: 100, opacity: 1, scale: 1 })
+
+//     .to("#square-group-3", { y: 0, duration: 100, opacity: 1, scale: 1 })
+//     // line
+//     .to("#square-group-4", { y: 0, duration: 100, opacity: 1, scale: 1 })
+//   }
+// })
+
+
 
 
 // gsap.set("#step-1-square", { x: 200, y: 0 , opacity:0, scale:0.2})
@@ -40,3 +87,29 @@ tl.to("#step-1-square", { x: 0, duration: 6, opacity:1, scale:1})
 // .to("#step-2-square", { x: 0, duration: 6, opacity:1, scale:1 })
 // .to("#step-3-square", { x: 0, duration: 6, opacity:1, scale:1 })
 // .to("#step-4-square", { x: 0, duration: 6, opacity:1, scale:1 })
+
+gsap.registerPlugin(ScrollTrigger);
+
+const squares = document.querySelectorAll(".part_8");
+
+squares.forEach(square => {
+  const squareTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: square,
+      start: "top top",
+      end: "bottom bottom",
+      toggleActions: "reverse play reverse play reverse",
+      markers: true
+    }
+  })
+
+  // Animate the content items
+  const squ = square.querySelectorAll(".squ");
+  squareTimeline.from(squ, {
+    duration: 1,
+    y: 100,
+    opacity: 0,
+    stagger: 0.3,
+  }, 0)
+
+});
